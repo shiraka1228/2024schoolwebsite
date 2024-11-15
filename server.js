7ba3130937d250629a10aa36dd15ee8090e39f30
shiraka1228 <lucy921228@gmail.com>
2024-11-15 12:05:35 +0800

    11152

--- server.js
+++ server.js
@@ -6,6 +6,21 @@ var server = express();
 //web root
 server.use(express.static(__dirname+"/1018/AgencyProject"));
 
+server.get("/services",(req,res)=>{
+//desk db
+var Services=[
+    
+];
+res.send(Services);
+
+});
+
+server.get("/profolio", (req,res)=>{       
+    //DB        
+    
+    
+res.send("Profolio");  });
+
 server.listen(80, ()=>{
     console.log("Server is running at port 80.");
 })
