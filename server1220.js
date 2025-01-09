e71af38282df19883dcb97f78e447528c8cb7c99
shiraka1228 <lucy921228@gmail.com>
2025-01-10 03:05:41 +0800

    1102

--- server1220.js
+++ server1220.js
@@ -3,15 +3,15 @@ var server = express();
 
 // 设置静态文件目录
 server.use(express.static(__dirname + "/web2024"));
+var DB = require("nedb-promises");
+var profoartDB = DB.create(__dirname+"/profoart.db");
 
-ProfolioDB.insert([
-    { imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
-    { imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
-    { imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" },
-    { imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
-    { imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
-    { imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" }
-])
+//profoartDB.insert([
+//   { imgUrl: "img2/cr3.PNG", ins: "作品1", h5: "BIRTHDAY", text: "彩虹社同人作品" },
+//   { imgUrl: "img2/fp06.png", ins: "作品2", h5: "少女レイ", text: "彩虹社同人作品" },
+//   { imgUrl: "img2/cr2.png", ins: "作品3", h5: "404!", text: "刀劍亂舞同人作品" },
+//   { imgUrl: "img2/pf05.png", ins: "作品4", h5: "CHRISTMAS", text: "刀劍亂舞同人作品" },
+//])
 
 
 
@@ -24,6 +24,37 @@ ProfolioDB.insert([
 //    ];
 //    res.json(art2); // 返回 JSON 格式的数据
 //});
+
+//server.get("/art3", (req, res)=>{
+//    //DB find
+//    var art2=[
+//        { imgUrl: "img2/cr3.PNG", ins: "作品1", h5: "BIRTHDAY", text: "彩虹社同人作品" },
+//        { imgUrl: "img2/fp06.png", ins: "作品2", h5: "少女レイ", text: "彩虹社同人作品" },
+//        { imgUrl: "img2/cr2.png", ins: "作品3", h5: "404!", text: "刀劍亂舞同人作品" },
+//        { imgUrl: "img2/pf05.png", ins: "作品4", h5: "CHRISTMAS", text: "刀劍亂舞同人作品" },
+//    ];
+//    res.send(art2);
+//});
+
+server.get("/art3", (req,res)=>{
+    //DB
+    profoartDB.find({}).then(results=>{
+      if(results != null){
+           res.send(results);
+      }else{
+          res.send("Error!");
+      }
+    })
+})
+
+
+
+
+
+//server.post("/art3", (req,res)=>{
+//    profoartDB.insert(req.body);
+//    res.redirect("/art2");
+//})
 
 // 启动服务器监听 80 端口
 server.listen(80, () => {
